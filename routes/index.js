96ae95b06db21b1241903c5c8bd1355ca7c0ea0b
woaiwyhty <398789568@qq.com>
2017-10-09 18:30:08 -0400

    index page router

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,43 @@ var passport = require('passport');
 var User = require('../dbOperations/user');
 var LocalStrategy = require('passport-local').Strategy;
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
   //res.render('index', { title: 'Goods Management System' });
-    res.redirect('/login');
+    res.render('main');
 });
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
 router.get('/login', function(req, res, next) {
     res.render('login', { title: 'Login'});
 });
+router.get('/main', function(req, res, next) {
+   res.render('main');
+});
 router.post('/login',
-    passport.authenticate('local', { failureRedirect: '/', failureFlash: "Invalid username or passport" }),
+    passport.authenticate('local',
+        { failureRedirect: '/', failureFlash: "Invalid username or passport",  successRedirect: '/main' }),
     function(req, res) {
         // If this function gets called, authentication was successful.
         // `req.user` contains the authenticated user.
         //req.flash("Log in successful");
-        res.redirect('/index');
+        res.redirect('/main');
     }
 );
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id); //store login status
+});
+
+passport.deserializeUser(function(id, done) {
+    User.getUserIDbyID(id, function(err, doc) {
+       done(null, doc);
+    });
+});
 
 passport.use(new LocalStrategy(function(username, password, done) {
     User.getUserNameByName(username, function(err, doc) {
@@ -33,5 +54,5 @@ passport.use(new LocalStrategy(function(username, password, done) {
         }
         return done(null, false, { message: "Incorrect password" });
     })
-}))
+}));
 module.exports = router;
