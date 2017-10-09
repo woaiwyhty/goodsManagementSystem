11e05577b592dd3812a79c33766fe21e1634d69c
woaiwyhty <398789568@qq.com>
2017-10-09 01:09:00 -0400

    users router

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.redirect('/users/login');
+
 });
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Login'});
+
+});
+
 
 module.exports = router;
