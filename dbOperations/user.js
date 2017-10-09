f32675431fc18df9140326a521e7e0dbbab0ebaa
woaiwyhty <398789568@qq.com>
2017-10-09 18:29:46 -0400

    operations in users collection

--- dbOperations/user.js
+++ dbOperations/user.js
@@ -3,11 +3,16 @@
  */
 var User = {
     getUserNameByName: function(username, callback) {
-        var userModel = global.dbHandle.getModel("users");
+        var userModel = global.dbHandle.getModel('users');
+        //console.log(username);
         userModel.findOne({username: username}, callback);
     },
     comparePassword: function(pwd, pwd1) {
         return pwd == pwd1;
+    },
+    getUserIDbyID: function(id, callback) {
+        var userModel = global.dbHandle.getModel('users');
+        userModel.findOne({id: id}, callback);
     }
 };
 
