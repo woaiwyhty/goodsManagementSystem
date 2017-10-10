81d2604f4468104b1802b0c28135e53804b3c0e7
woaiwyhty <398789568@qq.com>
2017-10-10 00:45:51 -0400

    database schema

--- dbModels/userModel.js
+++ dbModels/userModel.js
@@ -5,7 +5,37 @@ module.exports = {
     users: {
         username: {type: String, required: true},
         password: {type: String, required: true},
-        role: {type: Number},
+        role: {type: Number},// 0 means super admin, 1 means department admin, 2 means simple admin
         department: {type: Number}
     },
+    department: {
+        name: {type: String},
+        departmentAdminCnt: {type: Number},
+        simpleEmployeeCnt:  {type: Number},
+        goodsCnt: {type: Number},
+        departmentId: {type: Number}
+    },
+    supplier: {
+        name: {type: String},
+        phone: {type: String},
+        addr: {type: String},
+        postCode: {type: String},
+        supplyProductId: {type: String},
+        supplierId: {type: Number}
+    },
+    productsStock:  {
+        name: {type: String},
+        supplierId: {type: Number},
+        productId: {type: Number},
+        positionId: {type: Number},
+        count: {type: Number},
+        stockData: {type: Date},
+        stockId: {type: Number}
+    },
+    productInformation: {
+        name: {type: String},
+        productId: {type: Number},
+        price: {type:  Number},
+        productType: {type: String}
+    }
 };
