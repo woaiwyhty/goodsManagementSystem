061c9f1997f2f7afe2a70b96837efc3045db649a
woaiwyhty <398789568@qq.com>
2017-10-11 02:10:19 -0400

    index page router

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,47 @@ var router = express.Router();
 var passport = require('passport');
 var User = require('../dbOperations/user');
 var LocalStrategy = require('passport-local').Strategy;
+
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
   //res.render('index', { title: 'Goods Management System' });
-    res.render('main');
+    res.redirect('/main');
 });
 function ensureAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
+    if(req.session.user){
         return next();
     }
     res.redirect('/login');
 }
-
-router.get('/login', function(req, res, next) {
+function ensureAuthenticatedInLogin(req, res, next) {
+    if(!req.session.user) {
+        return next();
+    }
+    res.redirect('/main');
+}
+router.get('/login', ensureAuthenticatedInLogin, function(req, res, next) {
     res.render('login', { title: 'Login'});
 });
-router.get('/main', function(req, res, next) {
-   res.render('main');
+router.get('/main', ensureAuthenticated, function(req, res, next) {
+
+   res.render('main', { UsersArr: [{username: "woaiwyhty", authority: "super admin", department: "admin"}],
+                        mainTitle: "Welcome to Dashboard", loginName: req.session.user.username});
 });
+
 router.post('/login',
     passport.authenticate('local',
-        { failureRedirect: '/', failureFlash: "Invalid username or passport",  successRedirect: '/main' }),
+        { failureRedirect: '/', failureFlash: "Invalid username or passport"}),
     function(req, res) {
         // If this function gets called, authentication was successful.
         // `req.user` contains the authenticated user.
         //req.flash("Log in successful");
+        //res.redirect('/main');
+        req.session.user = req.user || null;
         res.redirect('/main');
     }
 );
+
+
 
 passport.serializeUser(function(user, done) {
     done(null, user.id); //store login status
