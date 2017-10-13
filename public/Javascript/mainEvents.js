d16f854798733351a536baa2614c039c82351309
woaiwyhty <398789568@qq.com>
2017-10-13 01:43:13 -0400

    event handlers

--- public/Javascript/mainEvents.js
+++ public/Javascript/mainEvents.js
@@ -3,8 +3,23 @@
  */
 var messagesArr = ['操作成功'];
 $(document).ready(function() {
+    $( "#dialog-form" ).dialog({
+        autoOpen: false,
+        height: 350,
+        width: 350,
+        modal: true,
+        buttons: {
+            "create a user": function() {
+                $( this ).dialog( "close" );
+            },
+            Cancel: function() {
+                $( this ).dialog( "close" );
+            }
+        },
+    });
     $('a.addUser').click(function() {
         alert('addUser clicked');
+        $('#dialog-form').dialog('open');
     });
     $('a.removeUser').click(function() {
         var id = $(this).attr('id');
