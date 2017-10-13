40280fa3d319db7f36ae1ae700b95a68547c5d66
woaiwyhty <398789568@qq.com>
2017-10-13 17:43:14 -0400

    department collection operations

--- dbOperations/department.js
+++ dbOperations/department.js
@@ -1,3 +1,17 @@
 /**
  * Created by dell on 2017/10/13.
  */
+
+var Department = {
+    getAllDepartments: function(callback) {
+        var model = global.dbHandle.getModel('department');
+        //console.log(username);
+        model.find({}, callback);
+    },
+    addDepartment: function(info, callback) {
+        var model = global.dbHandle.getModel('department');
+        model.create(info, callback);
+    }
+};
+
+module.exports = User;
