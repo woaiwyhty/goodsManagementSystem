622134ab8d142405727f181b47c018a304d3564d
woaiwyhty <398789568@qq.com>
2017-10-12 23:12:54 -0400

    parse the data format got from the database

--- dbDataParser/parser.js
+++ dbDataParser/parser.js
@@ -1,3 +1,12 @@
 /**
  * Created by dell on 2017/10/12.
  */
+var dataParser = {
+    authArr: ['Super Administrator', 'Department Administrator', 'Employee'],
+    getAuthority: function(numAuth) {
+        if(numAuth < 1) return authArr[0];
+        else if(numAuth < 2) return authArr[1];
+        else return authArr[2];
+    }
+};
+module.exports = dataParser;
