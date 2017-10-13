6a49b9dd2b4df9d817b541acdafa4cbd6ef2dc41
woaiwyhty <398789568@qq.com>
2017-10-13 00:21:06 -0400

    event handlers

--- public/Javascript/mainEvents.js
+++ public/Javascript/mainEvents.js
@@ -1,7 +1,18 @@
 /**
  * Created by dell on 2017/10/12.
  */
+var messagesArr = ['操作成功'];
 $(document).ready(function() {
-
+    $('a.addUser').click(function() {
+        alert('addUser clicked');
+    });
+    $('a.removeUser').click(function() {
+        var id = $(this).attr('id');
+        var username = $('.Userslist tr:eq(' + id + ') td:eq(1)').text();
+        $.post('/removeUser', { username: username}, function(res) {
+            alert(messagesArr[res.retCode]);
+            window.location.reload();
+        })
+    })
 });
 
