9b1df4272a01c54ac482d34bda23fd6d66ef43d4
woaiwyhty <398789568@qq.com>
2017-10-14 18:38:54 -0400

    department collection operations

--- dbOperations/department.js
+++ dbOperations/department.js
@@ -1,16 +1,19 @@
 /**
  * Created by dell on 2017/10/13.
  */
-
+var dbHandle = require("../dbModels/dbHandles");
+var model = dbHandle.getModel('departments');
+var ids = dbHandle.getModel('ids');
 var Department = {
     getAllDepartments: function(callback) {
-        var model = global.dbHandle.getModel('departments');
         //console.log(username);
         model.find({}, callback);
     },
     addDepartment: function(info, callback) {
-        var model = global.dbHandle.getModel('departments');
         model.create(info, callback);
+    },
+    idNumberInc: function(callback) {
+        ids.findOneAndUpdate({name: 'departments'}, {$inc: {idNumber: 1}}, {new: true}, callback);
     }
 };
 
