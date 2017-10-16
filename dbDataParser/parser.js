be4162e76a1500121d4616e4b5becd3c16f34e13
woaiwyhty <398789568@qq.com>
2017-10-15 22:59:29 -0400

    data parser

--- dbDataParser/parser.js
+++ dbDataParser/parser.js
@@ -2,11 +2,16 @@
  * Created by dell on 2017/10/12.
  */
 var authArr = ['Super Administrator', 'Department Administrator', 'Employee'];
+var productTypeArr = ['Rx', 'OTC'];
 var dataParser = {
     getAuthority: function(numAuth) {
         if(numAuth < 1) return authArr[0];
         else if(numAuth < 2) return authArr[1];
         else return authArr[2];
+    },
+    getProductType: function(num) {
+        if(num < 1) return productTypeArr[0];
+        else return productTypeArr[1];
     }
 };
 module.exports = dataParser;
